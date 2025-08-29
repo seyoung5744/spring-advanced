9638258eb2d72ec819d0ddf02926443398912b11
seyoung5744 <seyoung574458@gmail.com>
2025-08-29 15:36:34 +0900

    레벨 2-3: 코드 개선 퀴즈 - Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,12 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d).*$",
+            message = "비밀번호에는 최소 1개의 대문자와 1개의 숫자가 포함되어야 합니다."
+    )
     private String newPassword;
 }

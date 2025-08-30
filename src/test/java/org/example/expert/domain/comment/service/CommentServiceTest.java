b63b9f83c6fe9343e5103190692e6a0be8ba5825
seyoung5744 <seyoung574458@gmail.com>
2025-08-30 18:06:53 +0900

    레벨 7: CommentService getComments 테스트

--- src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
+++ src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.comment.service;
 
+import org.assertj.core.groups.Tuple;
 import org.example.expert.domain.comment.dto.request.CommentSaveRequest;
+import org.example.expert.domain.comment.dto.response.CommentResponse;
 import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.comment.repository.CommentRepository;
@@ -10,14 +12,17 @@ import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
@@ -67,5 +72,38 @@ class CommentServiceTest {
 
         // then
         assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("댓글 목록을 조회한다.")
+    void success_getComments() {
+        // given
+        User user1 = new User("test1@test.com", "12345", UserRole.USER);
+        User user2 = new User("test2@test.com", "12345", UserRole.USER);
+
+        Comment comment1 = new Comment("contents1", user1, null);
+        Comment comment2 = new Comment("contents2", user1, null);
+        Comment comment3 = new Comment("contents3", user2, null);
+
+        given(commentRepository.findByTodoIdWithUser(anyLong()))
+                .willReturn(List.of(
+                        comment1,
+                        comment2,
+                        comment3
+                ));
+
+        long todoId = 1L;
+
+        // when
+        List<CommentResponse> commentResponses = commentService.getComments(todoId);
+
+        // then
+        assertThat(commentResponses)
+                .extracting("contents", "user.email")
+                .contains(
+                        Tuple.tuple("contents1", "test1@test.com"),
+                        Tuple.tuple("contents2", "test1@test.com"),
+                        Tuple.tuple("contents3", "test2@test.com")
+                );
     }
 }
